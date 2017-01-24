0ce3950fb78d61e5acf7a1d7f2962218f36e925e
Kenny <k.bienaime@hotmail.com>
2017-01-24 08:08:35 -0500

    first commit: Database connect website

---
+++ README.md
@@ -0,0 +1,9 @@
+#Movie Database Project
+
+Every week there will be an advancement to create a website
+that is a database for movies
+
+##WEEK 1 ONJECTIVES
+
+1.Do an ERD for the database
+2.Connect a database to a webpage
---
+++ Week1/RetrieveData/Main_page.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+public partial class Main_page : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack) {
+
+            LoadDataMovie();
+
+        };
+
+    }
+
+    private void LoadDataMovie() {
+
+        SqlConnection con = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\dbMovie_week1.mdf;Integrated Security=True");
+        con.Open();
+        SqlCommand cmd = new SqlCommand("SELECT * FROM movie", con);
+        SqlDataReader dr = cmd.ExecuteReader();
+        GridView_movie.DataSource = dr;
+        GridView_movie.DataBind();
+        con.Close();
+
+    }
+}
