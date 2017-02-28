c77f549b2b381dbb13e982973eaa92959417d675
Kenny <k.bienaime@hotmail.com>
2017-02-28 07:39:41 -0500

    login page done

---
+++ Week2/Ken_Movie/Login.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Login.aspx.cs" Inherits="Login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <script src="Script/jquery-3.1.1.js"></script>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Login ID="Login_Movie" runat="server" DestinationPageUrl="~/User_main.aspx" OnAuthenticate="Login_Movie_Authenticate"></asp:Login>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Week2/Ken_Movie/Login.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data.SqlClient;
+
+public partial class Login : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    private bool UserLogin(string un, string pw)
+    {
+        string CS = ConfigurationManager.ConnectionStrings["Movie_Database"].ConnectionString;
+        using (SqlConnection con = new SqlConnection(CS))
+        {
+            SqlCommand cmd = new SqlCommand("Login", con);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@username", un);
+            cmd.Parameters.AddWithValue("@password", pw);
+            con.Open();
+            string result = Convert.ToString(cmd.ExecuteScalar());
+            if(String.IsNullOrEmpty(result))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+
+    protected void Login_Movie_Authenticate(object sender, AuthenticateEventArgs e)
+    {
+        string un = Login_Movie.UserName;
+        string pw = Login_Movie.Password;
+        bool result = UserLogin(un, pw);
+
+        if (result)
+        {
+            e.Authenticated = true;
+            Session["username"] = un;
+        }
+        else
+        {
+            e.Authenticated = false;
+        }
+    }
+}
